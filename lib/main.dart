505ee6b2df61984fb1df7774029ea32c3c349436
prathamesh <prathamesh.chande@kotak.com>
2023-01-02 10:28:54 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -44,146 +44,148 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        //crossAxisAlignment: CrossAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        //crossAxisAlignment: CrossAxisAlignment.baseline,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: const Card(
-              color: Colors.blue,
-              // ),
-              elevation: 5,
-              // child: Container(
-              //   //width: 100,
-              //   width: double.infinity,
-              child: Text('chart'),
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          //crossAxisAlignment: CrossAxisAlignment.baseline,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: const Card(
+                color: Colors.blue,
+                // ),
+                elevation: 5,
+                // child: Container(
+                //   //width: 100,
+                //   width: double.infinity,
+                child: Text('chart'),
+              ),
             ),
-          ),
 
-          UserTransactions(),
-          // Card(
-          //   elevation: 20,
-          //   child: Container(
-          //     padding: EdgeInsets.all(10),
-          //     child: Column(
-          //       crossAxisAlignment: CrossAxisAlignment.end,
-          //       children: [
-          //         TextField(
-          //           decoration: InputDecoration(labelText: 'Title'),
-          //           controller: titleController,
-          //           // onChanged: (val) {
-          //           //   titleInput = val;
-          //           // },
-          //         ),
-          //         TextField(
-          //           decoration: InputDecoration(labelText: 'Amount'),
-          //           controller: amountController,
-          //           // onChanged: (val) {
-          //           //   amountInput = val;
-          //           // },
-          //         ),
-          //         ElevatedButton(
-          //           child: Text('Add Transaction'),
-          //           onPressed: () {
-          //             // print(titleInput);
-          //             // print(amountInput);
-          //             print(titleController.text);
-          //             print(amountController.text);
-          //           },
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
+            UserTransactions(),
+            // Card(
+            //   elevation: 20,
+            //   child: Container(
+            //     padding: EdgeInsets.all(10),
+            //     child: Column(
+            //       crossAxisAlignment: CrossAxisAlignment.end,
+            //       children: [
+            //         TextField(
+            //           decoration: InputDecoration(labelText: 'Title'),
+            //           controller: titleController,
+            //           // onChanged: (val) {
+            //           //   titleInput = val;
+            //           // },
+            //         ),
+            //         TextField(
+            //           decoration: InputDecoration(labelText: 'Amount'),
+            //           controller: amountController,
+            //           // onChanged: (val) {
+            //           //   amountInput = val;
+            //           // },
+            //         ),
+            //         ElevatedButton(
+            //           child: Text('Add Transaction'),
+            //           onPressed: () {
+            //             // print(titleInput);
+            //             // print(amountInput);
+            //             print(titleController.text);
+            //             print(amountController.text);
+            //           },
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
 
-          //widget here
-          // NewTransaction(),
+            //widget here
+            // NewTransaction(),
 
-          // const Card(
-          //   color: Colors.red,
-          //   // ),
-          //   elevation: 5,
-          //   // child: Container(
-          //   //   //width: 100,
-          //   //   width: double.infinity,
-          //   child: Text('chart'),
-          // ),
-          // const Card(
-          //   child: Text('chart2'),
-          // ),
-          // Column(children: <Widget>[
-          //   Card(),
-          //   Card(),
-          // ],
+            // const Card(
+            //   color: Colors.red,
+            //   // ),
+            //   elevation: 5,
+            //   // child: Container(
+            //   //   //width: 100,
+            //   //   width: double.infinity,
+            //   child: Text('chart'),
+            // ),
+            // const Card(
+            //   child: Text('chart2'),
+            // ),
+            // Column(children: <Widget>[
+            //   Card(),
+            //   Card(),
+            // ],
 
-          // Column(
-          //   children: transactions.map((tx) {
-          //     return Card(
-          //       child: Row(
-          //         children: <Widget>[
-          //           Container(
-          //             margin: EdgeInsets.symmetric(
-          //               vertical: 10,
-          //               horizontal: 15,
-          //             ),
-          //             padding: EdgeInsets.all(10),
-          //             decoration: BoxDecoration(
-          //                 border: Border.all(
-          //               color: Colors.purple,
-          //               width: 2,
-          //             )),
-          //             //amount
-          //             child: Text(
-          //               // 'A:' + tx.amount.toString(),
-          //               //'${tx.amount}',
-          //               '\$${tx.amount}',
-          //               style: TextStyle(
-          //                 fontWeight: FontWeight.bold,
-          //                 fontSize: 20,
-          //                 color: Colors.purple,
-          //               ),
-          //             ),
-          //           ),
-          //           Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: <Widget>[
-          //               Text(
-          //                 tx.title,
-          //                 style: TextStyle(
-          //                   fontWeight: FontWeight.bold,
-          //                   fontSize: 16,
-          //                   color: Colors.black,
-          //                 ),
-          //               ),
-          //               Text(
-          //                 // DateFormat('dd-MM-yyyy').format(tx.date),
-          //                 DateFormat.yMMMEd().format(tx.date),
-          //                 style: TextStyle(
-          //                   fontWeight: FontWeight.bold,
-          //                   fontSize: 14,
-          //                   color: Colors.grey,
-          //                 ),
-          //               ),
-          //             ],
-          //           )
-          //         ],
-          //       ),
-          //     );
-          //   }).toList(),
-          // ),
+            // Column(
+            //   children: transactions.map((tx) {
+            //     return Card(
+            //       child: Row(
+            //         children: <Widget>[
+            //           Container(
+            //             margin: EdgeInsets.symmetric(
+            //               vertical: 10,
+            //               horizontal: 15,
+            //             ),
+            //             padding: EdgeInsets.all(10),
+            //             decoration: BoxDecoration(
+            //                 border: Border.all(
+            //               color: Colors.purple,
+            //               width: 2,
+            //             )),
+            //             //amount
+            //             child: Text(
+            //               // 'A:' + tx.amount.toString(),
+            //               //'${tx.amount}',
+            //               '\$${tx.amount}',
+            //               style: TextStyle(
+            //                 fontWeight: FontWeight.bold,
+            //                 fontSize: 20,
+            //                 color: Colors.purple,
+            //               ),
+            //             ),
+            //           ),
+            //           Column(
+            //             crossAxisAlignment: CrossAxisAlignment.start,
+            //             children: <Widget>[
+            //               Text(
+            //                 tx.title,
+            //                 style: TextStyle(
+            //                   fontWeight: FontWeight.bold,
+            //                   fontSize: 16,
+            //                   color: Colors.black,
+            //                 ),
+            //               ),
+            //               Text(
+            //                 // DateFormat('dd-MM-yyyy').format(tx.date),
+            //                 DateFormat.yMMMEd().format(tx.date),
+            //                 style: TextStyle(
+            //                   fontWeight: FontWeight.bold,
+            //                   fontSize: 14,
+            //                   color: Colors.grey,
+            //                 ),
+            //               ),
+            //             ],
+            //           )
+            //         ],
+            //       ),
+            //     );
+            //   }).toList(),
+            // ),
 
-          //widget here
-          // TransactionList(),
-        ],
+            //widget here
+            // TransactionList(),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,60 +10,69 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                //amount
-                child: Text(
-                  // 'A:' + tx.amount.toString(),
-                  //'${tx.amount}',
-                  '\₹ ${tx.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+    return Container(
+      height: 300,
+      // child: SingleChildScrollView(
+      child: ListView.builder(
+        itemBuilder: (ctxp, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
                     color: Colors.purple,
+                    width: 2,
+                  )),
+                  //amount
+                  child: Text(
+                    // 'A:' + tx.amount.toString(),
+                    //'${tx.amount}',
+                    '\₹ ${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.black,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                  Text(
-                    // DateFormat('dd-MM-yyyy').format(tx.date),
-                    DateFormat.yMMMEd().format(tx.date),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.grey,
+                    Text(
+                      // DateFormat('dd-MM-yyyy').format(tx.date),
+                      DateFormat.yMMMEd().format(transactions[index].date),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        // children: transactions.map((tx) {
+
+        // }).toList(),
+      ),
+      // ),
     );
   }
 }
